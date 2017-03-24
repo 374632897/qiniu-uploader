cccd7ae9ddcf19621e417f8333f6875f73bd6fe3
Jason <374632897@qq.com>
2017-03-24 12:06:10 +0800

    fixed code style

--- client/utils/index.js
+++ client/utils/index.js
@@ -13,9 +13,6 @@ const readFile = (file, cb = function () {}) => {
 };
 
 const uploadBase64 = ({ pic, size, token, cb: function () {} }) => {
-  // var pic = '';// base64字符串
-      //imageFile.size为图片流的大小
-  // var url = "http://upload.qiniu.com/putb64/" + imageFile.size; // 上传域名
   var url = `${BASE_URL}/putb64/${size}`; // 上传域名
 
   var xhr = new XMLHttpRequest();
@@ -23,12 +20,6 @@ const uploadBase64 = ({ pic, size, token, cb: function () {} }) => {
   xhr.onreadystatechange = function() {
     if (xhr.readyState==4){
       cb(JSON.parse(xhr.responseText));
-      // // 成功
-      // console.log(xhr.responseText);
-      // //转换成json对象，包含key值，hash值。
-      // var dataImg = JSON.parse(xhr.responseText);
-      // //发送给服务端消息
-      // sendImg('paste', dataImg);
     }
   }
   xhr.open("POST", url, true);
--- client/utils/paste.js
+++ client/utils/paste.js
@@ -1,44 +1,17 @@
 const $pasteEle = document.getElementById('paste');
 
-$pasteEle.addEventListener('paste', function(e){
-    for (var i = 0; i < e.clipboardData.items.length; i++) {
-        // 检测是否为图片类型
-        if (e.clipboardData.items[i].kind == "file" && /image\//.test(e.clipboardData.items[i].type)) {
-            var file = e.clipboardData.items[i].getAsFile();
-            window.uploader.addFile(file);
-            // var fileReader = new FileReader();
-            // fileReader.onloadend = function(e) {
-            //     //图片的base64编码
-            //     console.log(e.target.result);
-
-            //     //这里设置获取token的数据
-            //     var thetoken = '';
-            //     var head = {
-            //         "cmd": 0x4000C,
-            //         "uid": parseInt(EXTUID),
-            //         "ver": 1,
-            //         "sid": SID,
-            //         "apptype":1,
-            //         "entry_type": 2
-            //         }
-            //     var rdata = {
-            //         "head":head
-            //         }
-            //     rdata = JSON.stringify(rdata);
-            //     //获取后台的给的token
-            //     $.post(request_url_server,rdata,function(data,status){
-            //         if (status == "success") {
-            //             if (data['ret'] == 0){
-            //                 var thetoken = data['token'];
-            //                 pubt64(e.target.result.replace('data:image/png;base64,',''), imageFile, thetoken);
-            //             }
-            //         }
-            //     });
-            // };
-            //  //将文件读取为DataUrl
-            // fileReader.readAsDataURL(imageFile);
-            // e.preventDefault();
-            break;
-        }
+$pasteEle.addEventListener('paste', (e) => {
+  const items = e.clipboardData.items;
+  for (let i = 0; i < items.length; i++) {
+    const item = items[i];
+    // 检测是否为图片类型
+    if (
+      item.kind == "file"
+      && /image\//.test(item.type)
+    ) {
+      var file = e.clipboardData.items[i].getAsFile();
+      window.uploader.addFile(file);
+      break;
     }
+  }
 });
